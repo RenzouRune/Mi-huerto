befbe6e80e308a18d5088b63ee012f1e715284ef
RenzouRune <robinson.barrientos.patricio@gmail.com>
2025-08-29 15:29:41 -0400

    mensaje perdone profe (por si acaso)

--- cultivos/urls.py
+++ cultivos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import listar_cultivos
+from .views import listar_cultivos, detalle_cultivo
 
 urlpatterns = [
-    path('', listar_cultivos, name='listar_cultivos')
+    path('', listar_cultivos, name='listar_cultivos'),
+    path('detalle/<str:nombre>/', detalle_cultivo, name='detalle_cultivo'),
 ]
--- cultivos/views.py
+++ cultivos/views.py
@@ -1,9 +1,60 @@
 from django.shortcuts import render
 
 def listar_cultivos(request):
-    datos = {
-        'titulo': 'Mi huerto',
-        'mensaje': 'Esta es la lista simulada de cultivos',
-        'cultivos': ['Tomates', 'Papas', 'Lechuga', 'Zanahoria']
+    cultivos_originales = [
+        {'nombre': 'Tomate', 'tipo': 'fruto'},
+        {'nombre': 'Lechuga', 'tipo': 'hoja'},
+        {'nombre': 'Albahaca', 'tipo': 'hoja'},
+        {'nombre': 'Zanahoria', 'tipo': 'raiz'},
+        {'nombre': 'Cilantro', 'tipo': 'hoja'},
+        {'nombre': 'Pimiento', 'tipo': 'fruto'},
+        
+    ]
+    termino = request.GET.get('buscar','')
+    tipo = request.GET.get('tipo', '')
+    cultivo_filtrados = cultivos_originales
+
+    if termino:
+        cultivo_filtrados = [c for c in cultivo_filtrados if termino.lower() in c['nombre'].lower()]
+    if tipo:
+        cultivo_filtrados = [c for c in cultivo_filtrados if tipo.lower() in c['tipo'] == tipo]
+
+    contexto = {
+        'titulo':'Mi huerto',
+        'mensaje':'Filtra los cultivos por nombre y tipo',
+        'cultivos': cultivo_filtrados,
     }
-    return render(request, 'cultivos/lista_cultivos.html', datos)
+    return render(request, 'cultivos/lista_cultivos.html', contexto)
+
+def detalle_cultivo(request, nombre):
+    cultivos = {
+        'Tomate':{
+            'tipo':'fruto',
+            'descripcion':'Requiere sol directo y riego moderado, muy sensible al frio',
+            'siembra':'Agosto - Octubre',
+            'cosecha':'Noviembre - Enero'
+        },
+        'Lechuga':{
+            'tipo':'hoja',
+            'descripcion':'Preferiblemente sombra y suelo humedo',
+            'siembra':'Marzo - Mayo',
+            'cosecha':'60 dias despues de ser sembrado'            
+        },
+        'Albahca':{
+            'tipo':'hoja',
+            'descripcion':'Prefiere climas calidos, no tolerancia a las heladas',
+            'siembra':'Septiembre - Noviembre',
+            'cosecha':'Noviembre - Enero'
+        },
+    }
+    cultivos = cultivos.get(nombre)
+    if not cultivos:
+        return render(request, 'cultivos/no_encontrado.html', {'nombre': nombre})
+    contexto = {
+        'nombre':nombre,
+        'tipo':cultivos['tipo'],
+        'descripcion':cultivos['descripcion'],
+        'siembra':cultivos['siembra'],
+        'cosecha':cultivos['cosecha'],
+    }
+    return render(request, 'cultivos/detalle.html', contexto)
